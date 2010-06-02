640528f564a894f8d6cf6ea4e151e675b1b66e6c
tiago.pasqualini <tiago.pasqualini@e1c4881a-0ca5-ba26-b7d4-d93e6cbf6cd5>
2010-06-02 17:30:54 +0000

    Subindo os arquivos iniciais do projeto...

---
+++ gestaocantina/src/Classes/DadosMes.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Classes;
+
+/**
+ *
+ * @author Katha
+ */
+public class DadosMes {
+    private double valorUnitCusto;
+    private double valorUnitVenda;
+    private int qtdCompra;
+    private int qtdVenda;
+
+    public int getQtdCompra() {
+        return qtdCompra;
+    }
+
+    public void setQtdCompra(int qtdCompra) {
+        this.qtdCompra = qtdCompra;
+    }
+
+    public int getQtdVenda() {
+        return qtdVenda;
+    }
+
+    public void setQtdVenda(int qtdVenda) {
+        this.qtdVenda = qtdVenda;
+    }
+
+    public double getValorUnitCusto() {
+        return valorUnitCusto;
+    }
+
+    public void setValorUnitCusto(double valorCusto) {
+        this.valorUnitCusto = valorCusto;
+    }
+
+    public double getValorUnitVenda() {
+        return valorUnitVenda;
+    }
+
+    public void setValorUnitVenda(double valorVenda) {
+        this.valorUnitVenda = valorVenda;
+    }
+
+}
---
+++ gestaocantina/src/Classes/Produto.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Katha
+ */
+public class Produto {
+
+    private ArrayList<DadosMes> historico;
+    private String nome;
+    private int qtdAtual;
+    private int qtdSeguranca;
+
+    public Produto(){
+        historico = new ArrayList();
+    }
+    
+    public ArrayList<DadosMes> getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(ArrayList<DadosMes> historico) {
+        this.historico = historico;
+    }
+
+    public void addHistorico(DadosMes dadosMes) {
+
+        this.historico.add(dadosMes);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQtdAtual() {
+        return qtdAtual;
+    }
+
+    public void setQtdAtual(int qtdAtual) {
+        this.qtdAtual = qtdAtual;
+    }
+
+    public int getQtdSeguranca() {
+        return qtdSeguranca;
+    }
+
+    public void setQtdSeguranca(int qtdSeguranca) {
+        this.qtdSeguranca = qtdSeguranca;
+    }
+}
