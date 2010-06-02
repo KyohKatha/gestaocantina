6c0aeb5a0e1e3670f6f5cd5317946d453efc1d2f
tiago.pasqualini <tiago.pasqualini@e1c4881a-0ca5-ba26-b7d4-d93e6cbf6cd5>
2010-06-02 17:30:54 +0000

    Subindo os arquivos iniciais do projeto...

---
+++ trunk/gestaocantina/src/Classes/ACP.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Katha
+ */
+public class ACP {
+    private ArrayList<Produto> produtos;
+
+    public void gerarABC(){
+
+    }
+
+    public void MRP(){
+
+    }
+    
+    public void loteEconomicoPedidos(){
+
+    }
+
+    public void revisaoContinua(){
+
+    }
+
+    public void sistemasRevisaoPeriodica(){
+
+    }
+}
+
